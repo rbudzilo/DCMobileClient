6ca2514ae690180e35781d4c7d5e690d6883a34d
budzilo <roman@budzilo.ru>
2021-09-22 23:06:52 +0300

    Кол-во фрагментов ограничено двумя

--- app/src/main/java/com/budzilo/dcmobileclient/NumberAdapter.kt
+++ app/src/main/java/com/budzilo/dcmobileclient/NumberAdapter.kt
@@ -6,7 +6,7 @@ import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
 class NumberAdapter(fragment: FragmentActivity) : FragmentStateAdapter(fragment) {
-    override fun getItemCount(): Int = 100
+    override fun getItemCount(): Int = 2
 
     override fun createFragment(position: Int): Fragment {
         val fragment = BlankFragment()
