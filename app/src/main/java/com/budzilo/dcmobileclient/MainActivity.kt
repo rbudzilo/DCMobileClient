461cc8159f1904e6e54a337283e4d782dd2f8e0d
budzilo <roman@budzilo.ru>
2021-09-22 23:48:02 +0300

    Добавлен TabLayout

--- app/src/main/java/com/budzilo/dcmobileclient/MainActivity.kt
+++ app/src/main/java/com/budzilo/dcmobileclient/MainActivity.kt
@@ -3,10 +3,17 @@ package com.budzilo.dcmobileclient
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
     private lateinit var adapter: NumberAdapter
     private lateinit var viewPager: ViewPager2
+    private lateinit var tabLayout: TabLayout
+    private val tabNames: Array<String> = arrayOf(
+        "Вход",
+        "Регистрация",
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,5 +22,10 @@ class MainActivity : AppCompatActivity() {
         adapter = NumberAdapter(this)
         viewPager = findViewById(R.id.pager)
         viewPager.adapter = adapter
+
+        tabLayout = findViewById(R.id.tab_layout)
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = tabNames[position]
+        }.attach()
     }
 }
